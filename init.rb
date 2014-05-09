82b4f6cf5a89d5e25c679b9b8a4b4af1a1b9cf93
Peter Soetens <peter@thesourceworks.com>
2014-05-09 15:58:40 +0200

    init: point to github instead of gitorious
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- init.rb
+++ init.rb
@@ -1,23 +1,21 @@
-gitorious_long_doc = [
-    "Access method to gitorious (git, http or ssh)",
-    "Use 'ssh' only if you have a gitorious account and have commit rights",
-    "on the Orocos projects. Otherwise, we advise you to use 'git'"]
+github_long_doc = [
+    "Access method to github (https or ssh)",
+    "Use 'ssh' only if you have a github account and have commit rights",
+    "on the Orocos projects. Otherwise, we advise you to use 'https'"]
 
-configuration_option 'GITORIOUS', 'string',
-    :default => "git",
-    :possible_values => ['git', "http", "ssh"],
-    :doc => gitorious_long_doc do |value|
+configuration_option 'GITHUB', 'string',
+    :default => "https",
+    :possible_values => ["https", "ssh"],
+    :doc => github_long_doc do |value|
 
-    if value == "git"
-        Autoproj.change_option("GITORIOUS_ROOT", "git://gitorious.org/")
-    elsif value == "http"
-        Autoproj.change_option("GITORIOUS_ROOT", "http://git.gitorious.org/")
+    if value == "https"
+        Autoproj.change_option("GITHUB_ROOT", "https://github.com/")
     elsif value == "ssh"
-        Autoproj.change_option("GITORIOUS_ROOT", "git@gitorious.com:")
+        Autoproj.change_option("GITHUB_ROOT", "git@github.com:")
     end
 
     value
 end
 
-Autoproj.user_config('GITORIOUS')
+Autoproj.user_config('GITHUB')
 
