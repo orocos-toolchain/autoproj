52945caff3b55a6672e36fde3d8b6ed0a433c559
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-05-16 16:39:54 +0200

    use the autoproj helpers to get github and gitorious definitions

--- init.rb
+++ init.rb
@@ -1,21 +1,7 @@
-github_long_doc = [
-    "Access method to github (https or ssh)",
-    "Use 'ssh' only if you have a github account and have commit rights",
-    "on the Orocos projects. Otherwise, we advise you to use 'https'"]
-
-configuration_option 'GITHUB', 'string',
-    :default => "https",
-    :possible_values => ["https", "ssh"],
-    :doc => github_long_doc do |value|
-
-    if value == "https"
-        Autoproj.change_option("GITHUB_ROOT", "https://github.com/")
-    elsif value == "ssh"
-        Autoproj.change_option("GITHUB_ROOT", "git@github.com:")
-    end
-
-    value
+require 'autoproj/gitorious'
+if !Autoproj.has_source_handler? 'gitorious'
+    Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 end
-
-Autoproj.user_config('GITHUB')
-
+if !Autoproj.has_source_handler? 'github'
+    Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
+end
